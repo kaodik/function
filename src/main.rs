f631d357f9d3ecf5139e06b97e24d6650505b715
kao <trumblays@yahoo.com>
2023-01-22 21:18:47 -0600

    add functions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
     println!("Hello, world!");
+    another_function(5);
+    print_labeled_measurement(5,'h');
 }
+fn another_function(x:i32){
+    println!("Another funcition.");
+    println!("The value of x is: {x}");
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char){
+    println!("The measurment is: {value}{unit_label}");
+}
+
+//main function is the entry point of many programs
+
